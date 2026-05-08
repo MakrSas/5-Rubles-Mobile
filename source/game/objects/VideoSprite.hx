dc0486494d1765fe23ea209da6ba83924dcd638b
Makr <58633902+MakrSas@users.noreply.github.com>
2026-05-08 22:39:18 +0300

    Update VideoSprite.hx

--- source/game/objects/VideoSprite.hx
+++ source/game/objects/VideoSprite.hx
@@ -1,6 +1,6 @@
 package game.objects;
 
-#if (VIDEOS_ALLOWED && hxvlc)
+#if VIDEOS_ALLOWED
 import flixel.graphics.FlxGraphic;
 import flixel.math.FlxMath;
 import flixel.util.FlxColor;
@@ -10,7 +10,7 @@ import haxe.Int64;
 import haxe.io.Bytes;
 import haxe.io.Path;
 
-//import hxvlc.externs.Types;
+import hxvlc.externs.Types;
 import hxvlc.openfl.Video;
 import hxvlc.util.Location;
 #if (hxvlc >= version("2.1.0"))
